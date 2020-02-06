288d6bda84c14affde8141e6fcb1ace009f8552d
Andrew J Casarsa <acasarsa@gmail.com>
2020-02-06 03:00:07 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -37,7 +37,7 @@ def print_first_directors_movie_titles
   movie_index = 0
 
   while movie_index < spielburg_movies.length do
-    puts spielburg_movies[0][:title]
+    puts spielburg_movies[movie_index][:title]
     movie_index += 1
   end
 

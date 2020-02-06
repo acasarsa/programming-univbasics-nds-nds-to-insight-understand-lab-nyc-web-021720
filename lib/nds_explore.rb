3d06302bd0360c66f8f668a1cbc2c01fc20af914
Andrew J Casarsa <acasarsa@gmail.com>
2020-02-06 02:41:10 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -4,8 +4,43 @@ require 'directors_database'
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-  nil
+  pp nds
+  binding.pry
 end
 
+# def print_first_directors_movie_titles
+#   director_index = 0
+#   nds = directors_database
+#
+#   while director_index < nds.length do
+#     puts "start array"
+#     puts nds[director_index][:movies]
+#     puts "ends array"
+#
+#     current_directors_movies = nds[director_index][:movies]
+#     movie_index = 0
+#     while movie_index < current_directors_movies.length do
+#       puts current_directors_movies[movie_index][:title]
+#       movie_index += 1
+#
+#     end
+#
+#     director_index += 1
+#   end
+#
+# end
+
 def print_first_directors_movie_titles
+  ddb = directors_database
+  # movie_index = 0
+  movie_index = 0
+
+  while movie_index < ddb[0][:movies].length do
+    puts movie_index
+
+  end
+  movie_index += 1
+
 end
+
+# ddb[0][:movies][0]
